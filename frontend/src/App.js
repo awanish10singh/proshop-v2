931eea64feefa3ddf41ba44b4bc9ee39bc655d49
awanish10singh <awanishsudoku@gmail.com>
2024-02-14 19:43:51 +0530

    added backedn and frontend functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+import Header from "./componenets/Header";
+import Footer from "./componenets/Footer";
 
 const App = () => {
-  return <h1>Welcome to ProShop2</h1>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
